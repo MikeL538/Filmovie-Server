453194432788a48fac4a7929d3c7dc7e89de087a
Mikel538 <mikel538.work@gmail.com>
2026-02-11 09:17:30 +0100

    Initial push: project configured and structured

---
+++ prisma.config.ts
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import { defineConfig } from "prisma/config";
+
+// Pobranie zmiennej środowiskowej
+const DATABASE_URL = process.env.DATABASE_URL;
+
+if (!DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined");
+}
+
+// Konfiguracja Prisma
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: DATABASE_URL,
+  },
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Filmoteka Server
+
+An API for login, registration, getting lists and so on for Filmoteka: https://github.com/MikeL538/Filmoteka
